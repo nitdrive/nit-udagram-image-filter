0739b0dc695b25caeb75a02d876f7bc8e5fac4f6
Nithin <nithin.ayiluri@gmail.com>
2020-04-17 01:59:10 -0500

    Adds error handling to handle cases where file url is not valid

--- src/server.ts
+++ src/server.ts
@@ -23,25 +23,29 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
       const image_url: string = req.query.image_url;
       
       //1). Validate the image_url query.
-      if(image_url) {
-        
+      if(!image_url) {
+        res.status(400);
+        res.send('Image url cannot be empty');
+      } else {
         //2). Call filterImageFromURL(image_url) to filter the image.
         const tempPath: string = await filterImageFromURL(image_url);
-
-        // 3). Send the resulting file in the response.
-        res.sendFile(tempPath);
-
-        // 4). Delete local file after response is done.
-        res.on('finish', function() {
-          try{
-            deleteLocalFiles([tempPath]);
-          } catch(e) {
-            console.log("Could not remove file at: "+ tempPath);
-          }
-        });
-        
-      } else {
-        res.send('Image url cannot be empty');
+  
+        if(!tempPath) {
+          res.status(422);
+          res.send("Could not process image with url: "+ image_url);
+        } else {
+           // 3). Send the resulting file in the response.
+          res.sendFile(tempPath);
+     
+          // 4). Delete local file after response is done.
+          res.on('finish', function() {
+            try {
+              deleteLocalFiles([tempPath]);
+            } catch(e) {
+              console.log("Could not remove file at: "+ tempPath);
+            }
+          }); 
+        }
       }
   });
 
--- src/util/util.ts
+++ src/util/util.ts
@@ -9,17 +9,24 @@ import Jimp = require('jimp');
 // RETURNS
 //    an absolute path to a filtered image locally saved file
 export async function filterImageFromURL(inputURL: string): Promise<string>{
-    return new Promise( async resolve => {
-        const photo = await Jimp.read(inputURL);
-        const outpath = '/tmp/filtered.'+Math.floor(Math.random() * 2000)+'.jpg';
-        await photo
-        .resize(256, 256) // resize
-        .quality(60) // set JPEG quality
-        .greyscale() // set greyscale
-        .write(__dirname+outpath, (img)=>{
-            resolve(__dirname+outpath);
-        });
+   
+    const result:string = await new Promise( async (resolve) => {
+        try{
+            const photo = await Jimp.read(inputURL);
+            const outpath = '/tmp/filtered.'+Math.floor(Math.random() * 2000)+'.jpg';
+            await photo
+            .resize(256, 256) // resize
+            .quality(60) // set JPEG quality
+            .greyscale() // set greyscale
+            .write(__dirname+outpath, (img)=>{
+                resolve(__dirname+outpath);
+            });
+        } catch(e) {
+            console.log("Jimp.read error for url:" + inputURL);
+            resolve(null);
+        }
     });
+    return result;
 }
 
 // deleteLocalFiles
