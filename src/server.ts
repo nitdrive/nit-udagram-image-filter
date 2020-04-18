440bd7eb22a3a226696945fbe9356930d9caff51
Nithin <nithin.ayiluri@gmail.com>
2020-04-18 03:44:32 -0500

    Adds http://udagram.greetings-time.com as allowed origin

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,7 @@ import { requireAuth } from './auth.router';
 
   //CORS Should be restricted
   app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "udagram.greetings-time.com");
+    res.header("Access-Control-Allow-Origin", "http://udagram.greetings-time.com");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
   });
